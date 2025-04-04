b9fd63979a82eb478d2e34d72aeda7e2c1c89d9c
zhtianyi <1294986966@qq.com>
2025-04-04 00:08:10 -0400

    shared_mem 版本

--- new_forward.cu
+++ new_forward.cu
@@ -4,8 +4,9 @@ namespace eecs471 {
 
 /*Self-defined Parameters*/
 #define TILE_WIDTH 16
-#define MAXKernelLength 24*12*7*7
-
+// #define MAXKernelLength 24*12*7*7
+#define MAX_C 12
+#define MAX_K 7
 // __global__ void forward_kernel(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K)
 // {
 
@@ -53,58 +54,67 @@ namespace eecs471 {
 
 
 /*Working Version*/
-__constant__ float k[MAXKernelLength]; 
+// __constant__ float k[MAXKernelLength]; 
 
 
-__global__ void forward_kernel(float *y, const float *x, const int B, const int M, const int C, const int H, const int W, const int K)
+__global__ void forward_kernel(float *y, const float *x, const float *k, const int B, const int M, const int C, const int H, const int W, const int K)
 {
-
-    /*
-    Modify this function to implement the forward pass described in Chapter 16.
-    We have added an additional dimension to the tensors to support an entire mini-batch
-    The goal here is to be correct AND fast.
-    We have some nice #defs for you below to simplify indexing. Feel free to use them, or create your own.
-    */
-
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
 
-// An example use of these macros:
-// float a = y4d(0,0,0,0)
-// y4d(0,0,0,0) = a
 #define y4d(i3, i2, i1, i0) y[(i3) * (M * H_out * W_out) + (i2) * (H_out * W_out) + (i1) * (W_out) + i0]
 #define x4d(i3, i2, i1, i0) x[(i3) * (C * H * W) + (i2) * (H * W) + (i1) * (W) + i0]
 #define k4d(i3, i2, i1, i0) k[(i3) * (C * K * K) + (i2) * (K * K) + (i1) * (K) + i0]
 
-    int W_grid = (W_out + TILE_WIDTH - 1) / TILE_WIDTH;        //number of horizontal tiles per output map
+    const int X_tile_width = TILE_WIDTH + K - 1;
+    // 声明共享内存，为每个通道分配空间
+    __shared__ float shared_kernel[MAX_C][MAX_K][MAX_K]; 
+    __shared__ float shared_input[MAX_C][TILE_WIDTH + MAX_K - 1][TILE_WIDTH + MAX_K - 1];
 
-    /*Self-defined Parameters*/
-    int m = blockIdx.x;                 // for each output feature maps
-    int h = (blockIdx.y / W_grid) * TILE_WIDTH + threadIdx.y;   //thread height in output featured map
-    int w = (blockIdx.y % W_grid) * TILE_WIDTH + threadIdx.x;   //thread width in output featured map
+    int W_grid = (W_out + TILE_WIDTH - 1) / TILE_WIDTH;
+
+    // int m = blockIdx.x / C;
+    // int c = blockIdx.x % C;
+
+    int m = blockIdx.x;
+    int h_base = (blockIdx.y / W_grid) * TILE_WIDTH;
+    int w_base = (blockIdx.y % W_grid) * TILE_WIDTH;
+    int h = h_base + threadIdx.y;
+    int w = w_base + threadIdx.x;
     int b = blockIdx.z;
-    float acc;
+    float acc = 0.0f;
+    for(int c = 0; c < C; c++){
+    // 协作加载输入数据到共享内存
+        for (int i = h; i < h_base + X_tile_width && i < H; i += TILE_WIDTH) {
+            for (int j = w; j < w_base + X_tile_width && j < W; j += TILE_WIDTH) {
+                if (i < H && j < W) {
+                    shared_input[c][i - h_base][j - w_base] = x4d(b, c, i, j);
+                } 
+            }
 
-    /*Current Version*/
-    /*Every thread caculate part of one element in one output featured map (one input map)*/
-    if(m < M && h < H_out && w < W_out){
-        acc = 0.0f;  // 初始化acc
-        for (int c = 0; c < C; c++){
-            for (int p = 0; p < K; p++){
-                for (int q = 0; q < K; q++){
-                    int input_h = h + p;
-                    int input_w = w + q;
-                    acc += x4d(b, c, input_h, input_w) * k4d(m, c, p, q);
-                }
+        }
+
+
+        if (threadIdx.x < K && threadIdx.y < K) {
+                shared_kernel[c][threadIdx.y][threadIdx.x] = k4d(m, c, threadIdx.y, threadIdx.x);
+        }
+        __syncthreads();
+
+
+        for (int p = 0; p < K; p++){
+            for (int q = 0; q < K; q++){
+                acc += shared_input[c][threadIdx.y + p][threadIdx.x + q] * shared_kernel[c][p][q];
             }
         }
+        __syncthreads();
+    }
+    if(m < M && h < H_out && w < W_out){
         y4d(b, m, h, w) = acc;
     }
 
-
-#undef y4d
-#undef x4d
-#undef k4d
+    #undef y4d
+    #undef x4d
+    #undef k4d
 }
 
 torch::Tensor forward(const torch::Tensor &x, const torch::Tensor &w, int64_t M) {
@@ -141,22 +151,24 @@ torch::Tensor forward(const torch::Tensor &x, const torch::Tensor &w, int64_t M)
     //     throw std::runtime_error("Input dimensions exceed constant memory limits");
     // }
     // 将卷积核复制到常量内存
-    cudaMemcpyToSymbol(k, w.data_ptr<float>(), M * C * K * K * sizeof(float));
+    // cudaMemcpyToSymbol(k, w.data_ptr<float>(), M * C * K * K * sizeof(float));
     const int H_out = H - K + 1;
     const int W_out = W - K + 1;
+    
+    // 创建输出张量并初始化为0
     auto y = torch::empty({B, M, H_out, W_out}, x.options());
-
 
     int W_grid = (W_out + TILE_WIDTH - 1) / TILE_WIDTH;
     int H_grid = (H_out + TILE_WIDTH - 1) / TILE_WIDTH;
     int Y = W_grid * H_grid;
-    
+    // int M_C = M * C;
     dim3 gridDim(M, Y, B);
     dim3 blockDim(TILE_WIDTH, TILE_WIDTH, 1);
 
-    forward_kernel<<<gridDim, blockDim>>>(y.data_ptr<float>(), x.data_ptr<float>(), B, M, C, H, W, K);
+    forward_kernel<<<gridDim, blockDim>>>(y.data_ptr<float>(), x.data_ptr<float>(), w.data_ptr<float>(), B, M, C, H, W, K);
     cudaDeviceSynchronize();
 
     return y;
 }
 }; // namespace eecs471
+
